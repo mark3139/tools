79d88de9f1a51b8a431cceeea98df5b3784f2148
mark3139 <yibomark@gmail.com>
2013-03-26 14:04:07 +0800

    add log

--- misc.py
+++ misc.py
@@ -2,3 +2,25 @@ def str_md5(content):
     import hashlib
     m = hashlib.md5(content)
     return m.hexdigest()
+
+
+def setlog(logname='', filename=None, console=True):
+    import logging
+    logger = logging.getLogger(logname)
+    logger.setLevel(logging.DEBUG)
+
+    fmt = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+    #file handler
+    if filename:
+        fh = logging.FileHandler(filename)
+        fh.setLevel(logging.INFO)
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)
+
+    #console handler
+    if console:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(fmt)
+        logger.addHandler(ch)
+    return logger
